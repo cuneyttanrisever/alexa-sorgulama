0ca3bf9b6e4c8cf10eca7ce1e080db25908b0962
Cüneyt Tanrısever <55285425+cuneyttanrisever@users.noreply.github.com>
2021-08-29 22:31:19 +0300

    Update domain alesa_sira_sorgulama.py

--- domain alesa_sira_sorgulama.py
+++ domain alesa_sira_sorgulama.py
@@ -2,85 +2,78 @@
 import re
 import requests
 coder= "* Cüneyt TANRISEVER *"
-print "*"*len(coder)
-print coder
-print "*"*len(coder)
+print ("*"*len(coder))
+print (coder)
+print ("*"*len(coder))
 yaz=open("4milyon_alti_siteler.txt","w")
-yaz.close()
-yaz=open("4milyon_ustu_siteler.txt","w")
-yaz.close()
-oku=raw_input("Dosya adini gir = ")
-oku1=open(oku,"r").readlines()
+yaz1=open("4milyon_ustu_siteler.txt","w")
+sor=input("Dosya adini gir = ")
+oku=open(sor,"r").readlines()
 urller=[]
-ekle=[]
-alexsa=[]
+_4_m_alti=[]
+_4_m_ustu=[]
+for i in oku:
+	site=i.replace("http://","").replace("https://","").replace("/","").replace("\r","").replace("\n","")
+	urller.append(site)
 def cektir(domain):
     try:
         i=urller[domain]
-        url="http://data.alexa.com/data?cli=10&url=%s"%(i)
-
-
+        url="http://data.alexa.com/data?cli=10&url={}".format(i)
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.28 Safari/537.36' }
         rq=requests.session()
         rq.headers.update(headers)
         dex=rq.get(url).content
-        globalrank= re.findall("TEXT=\"(.*?)\"",dex)
-        ulkealexsa=re.findall("RANK=\"(.*?)\"",dex)
-        ulkeadi=re.findall("NAME=\"(.*?)\"",dex)
-        siteadi=re.findall("IDN=\"(.*?)\"",dex)
-        
-        
+        globalrank= re.findall("TEXT=\"(.*?)\"",str(dex))
+        ulkealexsa=re.findall("RANK=\"(.*?)\"",str(dex))
+        ulkeadi=re.findall("NAME=\"(.*?)\"",str(dex))
+        siteadi=re.findall("IDN=\"(.*?)\"",str(dex))
         if globalrank != [] and ulkealexsa != []:
             gbrank=int(str(globalrank).replace("\'","").replace("[","").replace("]",""))
             ulkerank=int(str(ulkealexsa[1]).replace("\'","").replace("[","").replace("]",""))
 ###################################################################################
             globalrank1=int(str(globalrank).replace("\'","").replace("[","").replace("]",""))," = dunya alexasi // "
-            ulkealexsa1=int(str(ulkealexsa[1]).replace("\'","").replace("[","").replace("]","")), " = ulke alexasi "
+            ulkealexsa1=" ulke alexasi = ",int(str(ulkealexsa[1]).replace("\'","").replace("[","").replace("]",""))
             ulkeadi1=" // ulke adi = ",str(ulkeadi).replace("\'","").replace("[","").replace("]","")
-            sitead=" // site adi = ",str(siteadi).replace("\'","").replace("[","").replace("]","")
+            sitead=" // site adi = ",str(siteadi).replace("\'","").replace("[","").replace("]","").replace("/","")
 ##################################################################################
-#print globalrank1 ,ulkealexsa1,ulkeadi1
             if gbrank and ulkerank !=[]:
                 if gbrank <=4000000 and ulkerank <=4000000:
                     duzen=globalrank1+ulkealexsa1+ulkeadi1+sitead
-                    ekle.append(duzen)
-                    print duzen
+                    _4_m_alti.append(duzen)
+                    print (duzen)
                 else:
-                    duzen1=globalrank1+ulkealexsa1+ulkeadi1+sitead
-                    alexsa.append(duzen1)
-                    print duzen1
+                    duzen=globalrank1+ulkealexsa1+ulkeadi1+sitead
+                    _4_m_ustu.append(duzen)
+                    print (duzen)
         
         else:
-            print "Site Siralamada yok"
+            print ("Site Siralamada yok")
     except ValueError:
         pass	
     except IndexError:
         pass
 def sirala():
-    dex1= sorted(ekle)  
-    dex2= sorted(alexsa)
+    dex1= sorted(_4_m_alti)  
+    dex2= sorted(_4_m_ustu)
     for i in dex1:
         d=str(i).replace("[","").replace("\'","").replace("]","").replace(",","")
         dd=d.replace("(","").replace(")","")
-        yaz=open("4milyon_alti_siteler.txt","a")
         yaz.write(str(dd)+"\n")
-        yaz.close()
+
     for i in dex2:
         d=str(i).replace("[","").replace("\'","").replace("]","").replace(",","")
         dd=d.replace("(","").replace(")","")
-        yaz=open("4milyon_ustu_siteler.txt","a")
-        yaz.write(str(dd)+"\n")
-        yaz.close()
-    print "sonuclar = 4milyon_alti_siteler.txt ve 4milyon_ustu_siteler.txt dosyalarina yazilmistir"
+        yaz1.write(str(dd)+"\n")
 
-for i in oku1:
-	dd=i.replace("http://","").replace("https://","").replace("/","").replace("\r","").replace("\n","")
-	urller.append(dd)
-def gdr():
+    print ("sonuclar = 4milyon_alti_siteler.txt ve 4milyon_ustu_siteler.txt dosyalarina yazilmistir")
+    yaz.close()
+    yaz1.close()
+
+def gonder():
 	for i in range(len(urller)):
 		cektir(i)
 	sirala()
 
-gdr()
+gonder()
 
         
